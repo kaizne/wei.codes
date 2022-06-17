caae3da4b0b412b2fe6f0f541aad2449222912fa
Kaizne <github@kaizne.com>
2022-06-16 23:12:39 -0700

    Format contact

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -6,7 +6,7 @@
 { #if visible === 'contact' }
 <div class='w-80'>
 <h2  in:fly='{{ delay: 1000, x: 200, duration: 2000 }}' 
-    class='text-2xl font-semibold mt-2 mb-1'>Contact</h2>
+     class='text-2xl font-semibold mt-2 mb-1'>Contact</h2>
 <div in:blur='{{ delay: 1000, duration: 2500 }}'>w54wei@uwaterloo.ca</div>
 </div>
 { /if }
